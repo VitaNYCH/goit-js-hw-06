72641208d610f9cebd4876009861b166c54feaf6
VitaNYCH <vitanychvoloda@yahoo.com>
2023-02-14 23:18:16 +0200

    Updated Task2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsEl = document.querySelector('#ingredients');
+
+const elements = ingredients.map(ingredient => {
+  const ingredItemEl = document.createElement('li');
+  ingredItemEl.textContent = ingredient;
+  ingredItemEl.classList.add('item');
+
+  return ingredItemEl;
+});
+
+
+console.log(elements);
+
+ingredientsEl.append(...elements);
+
