511c10fd55bedd35e657a3298707136ab1670144
VitaNYCH <vitanychvoloda@yahoo.com>
2023-02-19 14:45:30 +0200

    updated task9

--- js/task-09.js
+++ js/task-09.js
@@ -3,7 +3,8 @@
 
 
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)}`;
 }
 const randomColor = getRandomHexColor();
 
