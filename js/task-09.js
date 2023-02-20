f727fe256e9b730b44a60e02d1d230253386eea3
VitaNYCH <vitanychvoloda@yahoo.com>
2023-02-20 08:20:29 +0200

    Corrected task 9

--- js/task-09.js
+++ js/task-09.js
@@ -2,21 +2,26 @@
 
 
 
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)}`;
-}
-const randomColor = getRandomHexColor();
+
 
 const colorChangeBtn = document.querySelector('.change-color');
+// const bodyEl = document.querySelector('body');
+// const spanEl = bodyEl.querySelector('.color');
+
+colorChangeBtn.addEventListener('click', onColorChangeBtn);
 const bodyEl = document.querySelector('body');
 const spanEl = bodyEl.querySelector('.color');
 
-colorChangeBtn.addEventListener('click', onColorChangeBtn);
 
 function onColorChangeBtn(event) {
   
-  bodyEl.style.backgroundColor = randomColor; 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const randomColor = getRandomHexColor();
+
+
+  bodyEl.style.backgroundColor = randomColor;
   spanEl.textContent = randomColor;
 
 }
